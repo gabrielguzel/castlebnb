563740c3c6bcb7d8ffb487b625679bb77b445adb
GuzelGokhan <gokhan@medispring.be>
2020-07-21 21:05:12 +0200

    some other other changement

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,25 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end
+  
+  def create
+    @review = Review.new(params_review)
+    @castle = Castle.find(params[:castle_id])
+    @review.castle = @castle
+    if @review.save
+      flash[:success] = "Review successfully created"
+      redirect_to @review
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+
+  private
+
+  def params_review
+    params.require(:review).permit(:stars,:description)
+  end
+  
 end

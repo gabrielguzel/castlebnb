82598739b3ccbced334a2f05809400829baeb585
GuzelGokhan <gokhan@medispring.be>
2020-07-21 16:31:53 +0200

    some photos

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create]
   end
   resources :reservations, only: [:edit, :update, :delete]
-  root to: 'pages#home'
+  root to: 'castles#index'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

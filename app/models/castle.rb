d70740860ba5a05795aaf95ad7d6c80a8afd3b65
GuzelGokhan <gokhan@medispring.be>
2020-07-21 15:40:36 +0200

    model

--- app/models/castle.rb
+++ app/models/castle.rb
@@ -1,3 +1,5 @@
 class Castle < ApplicationRecord
   belongs_to :user
+  has_many :reservation
+  has_many :castle
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :castles
+  has_many :reservations
+  has_many :reviews
 end

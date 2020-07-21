9a4832a7bbae2b9e8a35b4b3db10a0dd60c51652
Sabina Pirvan <sabina2001ro@hotmail.com>
2020-07-21 20:31:35 +0200

    controllers done

---
+++ app/controllers/reservation_controller.rb
@@ -0,0 +1,43 @@
+class ReservationController < ApplicationController
+    def create
+        @reservation = reservation.new(reservation_params)
+        if @reservation.save
+          redirect_to castle_path(@castle)
+        else
+          render 'new'
+        end
+    end
+
+    def new
+        @reservation = reservation.new
+        end
+    end
+
+    def edit
+        @reservation 
+        end 
+    end 
+
+    def update
+            if @reservation.update_attributes(reservation_params)
+                flash[:success] = "reservation was successfully updated"
+                redirect_to reservation_url
+            else
+                flash[:error] = "Something went wrong"
+                render 'edit'
+            end
+        end
+
+    def destroy
+        @reservation = reservation.find(params[:id])
+        @reservation.destroy
+        redirect_to reservation_url
+    end
+
+  private
+
+    def reservation_params
+        params.require(:reservation).permit(:arrival_date, :departure_date, :reservation, :user)
+        end
+    end
+end
---
+++ app/controllers/review_controller.rb
@@ -0,0 +1,20 @@
+class ReviewController < ApplicationController
+    def create
+        @review = review.new(review_params)
+        if @review.save
+          redirect_to review_path(@review)
+        else
+          render 'new'
+        end
+    end
+
+    def new
+        @review = review.new
+    end
+
+  private
+
+    def review_params
+        params.require(:review).permit(:stars, :description, :castle, :user)
+        end
+    end

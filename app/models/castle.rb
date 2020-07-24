fde6a128e7cd1727f5e1f50b35ae494c0603110b
Sabina Pirvan <sabina2001ro@hotmail.com>
2020-07-24 17:57:17 +0200

    geocode added to castle.rb

--- app/models/castle.rb
+++ app/models/castle.rb
@@ -2,4 +2,7 @@ class Castle < ApplicationRecord
   belongs_to :user
   has_many :reservation
   has_many :castle
+
+  geocoded_by: address
+  after_validation: geocode, if will_save_change_to_address?
 end
